339a3bf15492fa5aa4ecbbfac1e9f8a0cf0d150a
Christopher Kennedy <chris@onebrightlight.com>
2021-03-02 12:25:14 -0800

    Initial commit
    Hello, world!

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [
+    './src/**/*.js',
+    './src/**/*.vue',
+    './templates/**/*.twig',
+    './templates/**/*.html',
+    './templates/**/*.css',
+    './templates/**/*.js'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ webpack.mix.js
@@ -0,0 +1,5 @@
+let mix = require('laravel-mix');
+
+mix.js('src/js/app.js', 'web/assets/js').postCss('src/css/app.css', 'web/assets/css', [
+  require("tailwindcss"),
+]);
